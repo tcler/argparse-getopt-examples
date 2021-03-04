ed0918a07db68906f5644d5b5fc4b499cff2d3df
Jianhong Yin <jiyin@redhat.com>
2021-03-04 12:56:35 +0800

    groovy: add a picocli-groovy example checksum.groovy
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

---
+++ groovy/picocli-groovy/checksum.groovy
@@ -0,0 +1,25 @@
+//'info.picocli.picocli' has been rename to 'info.picocli:picocli-groovy' since 4.*
+@Grab('info.picocli:picocli-groovy:4.6.1')
+@GrabConfig(systemClassLoader=true)
+//Annotation PicocliScript Deprecated, use PicocliScript2 instead
+@picocli.groovy.PicocliScript2
+
+import groovy.transform.Field
+import java.security.MessageDigest
+import static picocli.CommandLine.*
+
+@Parameters(arity="1", paramLabel="FILE", description="The file(s) whose checksum to calculate.")
+@Field File[] files
+
+@Option(names = ["-a", "--algorithm"], description = [
+        "MD2, MD5, SHA-1, SHA-256, SHA-384, SHA-512,",
+        "  or any other MessageDigest algorithm."])
+@Field String algorithm = "MD5"
+
+@Option(names= ["-h", "--help"], usageHelp= true, description= "Show this help message and exit.")
+@Field boolean helpRequested
+
+files.each {
+  println MessageDigest.getInstance(algorithm).digest(it.bytes).encodeHex().toString() + "\t" + it
+}
+
