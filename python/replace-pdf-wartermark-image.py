5f0a5dca33ebee06f73ec1b390587e1372ffbc97
Jianhong Yin <jiyin@redhat.com>
2022-11-04 21:05:53 +0800

    python: rename remove-wm.py to replace-pdf-wartermark-image.py
    and improve code/comment
    
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- python/replace-pdf-wartermark-image.py
+++ python/replace-pdf-wartermark-image.py
@@ -21,12 +21,21 @@ for page_index in range(len(pdf)):
             wmimg_xrefs.append(img[0])
             break
 
+    # make a small 100% transparent pixmap (of just any dimension)
     pix = fitz.Pixmap(fitz.csGRAY, (0, 0, 1, 1), 1)
-    pix.clear_with()
+    pix.clear_with()  # clear all samples bytes to 0x00
+
+    # insert new image just anywhere
     new_xref = page.insert_image(page.rect, pixmap=pix)
+
+    # copy over definition and stream of new image
     for img_xref in wmimg_xrefs:
         pdf.xref_copy(new_xref, img_xref)
+
+    # there now is a second /Contents object, showing new image
     cont_xrefs = page.get_contents()
+
+    # make sure that new /Contents(cont_xrefs[1]) is forgotten again
     page.set_contents(cont_xrefs[0])
     page.clean_contents()
 pdf.ez_save(f"{path.replace('.pdf','')}-no-wartermark.pdf", garbage=4)
