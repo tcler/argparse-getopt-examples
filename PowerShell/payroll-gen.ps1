4eb876aaf38ee2b3dc63b4cd6d200cdaae53aaf5
Jianhong Yin <jiyin@redhat.com>
2023-02-15 14:44:08 +0800

    PowerShell/payroll-gen.ps1: support multi-files
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- PowerShell/payroll-gen.ps1
+++ PowerShell/payroll-gen.ps1
@@ -4,7 +4,7 @@ param (
 	[int]$nameColumnIndex = 5,
 
 	[Parameter(Position = 0, ValueFromRemainingArguments = $true)]
-	[string]$oargs
+	[array]$oargs
 )
 
 $existingExcel = @()
@@ -16,7 +16,8 @@ function Stop-Excel
 }
 
 #变量声明
-if ($args.count -eq 0) {
+[array]$OrigExcelFiles = @()
+if ($oargs.count -eq 0) {
 	Add-Type -AssemblyName System.Windows.Forms
 
 	$FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{
@@ -27,93 +28,104 @@ if ($args.count -eq 0) {
 	}
 	$fileBrowserResult = $FileBrowser.ShowDialog()
 	if ($fileBrowserResult -eq 'OK') {
-		foreach ($file in @($FileBrowser.FileNames)) {
-			echo "$file"
-		}
+		$OrigExcelFiles = @($FileBrowser.FileNames)
 	}
-	$OrigExcelFile = @($FileBrowser.FileNames)[0]
 } else {
-	$OrigExcelFile = Resolve-Path @($oargs)[0]
+	$OrigExcelFiles = $oargs | % { Resolve-Path $_ }
 }
-if (!$OrigExcelFile) {
-	echo "【Note】您没有提供文件路径，请重新执行，并选择需要处理的文件。"
-	#cmd /c pause
-	Read-Host -Prompt "按任意键退出..."
+
+if (!$OrigExcelFiles) {
+	echo "【Note】您没有提供文件路径，请重新执行，并选择需要处理的文件。`n按任意键退出..."
+	cmd /c pause >$nil
 	Exit
+}
+foreach ($file in $OrigExcelFiles) {
+	echo "=> $file"
 }
 
 $firstDataRowIndex = $headRowsCount + 1
 $timeColumnIndex = 1
-
-echo "{info} open $OrigExcelFile ..."
 $Excel = New-Object -ComObject Excel.Application
 $Excel.DisplayAlerts = $false;
-$wb = $Excel.Workbooks.Open($OrigExcelFile)
-if (!$wb) {
-	echo "{ERROR} open Excel file failed, please check the file format."
-	$Excel.Quit()
-	Stop-Excel
-	Exit
-}
 
-$ws = @($wb.Worksheets)[0]
-echo "{debug} $($ws.Cells.Item(1,5).text)"
-echo "{debug} $($ws.Cells.Item(2,5).text)"
-echo "{debug} $($ws.Cells.Item(3,5).text)"
-
-$rowcnt = ($ws.UsedRange.Rows).count
-$colcnt = ($ws.UsedRange.Colums).count
-
-echo "{info} create template worksheet ..."
-$ws.Copy($wb.Worksheets.Item(1))
-$wstmp = $wb.Worksheets.Item(1)
-$wstmp.Name = "tmpsheet"
-
-$tmprowcnt = ($wstmp.UsedRange.Rows).count
-while ($tmprowcnt -gt $firstDataRowIndex) {
-	for ($i = $firstDataRowIndex; $i -lt $tmprowcnt; $i++) {
-		[void]$wstmp.Cells.Item($i, 1).EntireRow.Delete()
+foreach ($OrigExcelFile in $OrigExcelFiles) {
+	echo "`n{info} open $OrigExcelFile ..."
+	$wb = $null
+	$wb = $Excel.Workbooks.Open($OrigExcelFile)
+	if (!$wb) {
+		echo "{ERROR} open Excel file failed, please check the file format."
+		[void]$Excel.Quit()
+		continue
 	}
+
+	$ws = @($wb.Worksheets)[0]
+	if (!$ws) {
+		echo "{ERROR} can not get worksheets, please check the file format."
+		$Excel.Workbooks.Close()
+		[void]$Excel.Quit()
+		continue
+	}
+	echo "{debug} $($ws.Cells.Item(1,5).text)"
+	echo "{debug} $($ws.Cells.Item(2,5).text)"
+	echo "{debug} $($ws.Cells.Item(3,5).text)"
+
+	$rowcnt = ($ws.UsedRange.Rows).count
+	$colcnt = ($ws.UsedRange.Colums).count
+
+	echo "{info} create template worksheet ..."
+	$ws.Copy($wb.Worksheets.Item(1))
+	$wstmp = $wb.Worksheets.Item(1)
+	$wstmp.Name = "tmpsheet"
+
 	$tmprowcnt = ($wstmp.UsedRange.Rows).count
-	echo "{debug} tempsheet row count: $tmprowcnt"
+	while ($tmprowcnt -gt $firstDataRowIndex) {
+		for ($i = $firstDataRowIndex; $i -lt $tmprowcnt; $i++) {
+			[void]$wstmp.Cells.Item($i, 1).EntireRow.Delete()
+		}
+		$tmprowcnt = ($wstmp.UsedRange.Rows).count
+		echo "{debug} tempsheet row count: $tmprowcnt"
+	}
+	echo "{info} origsheet row count: $rowcnt"
+	echo "{info} tempsheet row count: $tmprowcnt"
+
+	$time = $ws.Cells.Item($firstDataRowIndex, $timeColumnIndex).text
+	if ($time -eq "") {
+		echo "{ERROR} Time format is wrong, skip $OrigExcelFile"
+		continue
+	}
+	$payrollFolder = "$pwd\payroll-list-$time"
+	New-Item -Path $payrollFolder -ItemType Directory -ErrorAction Ignore >$null
+
+	$dstRowIdx = $firstDataRowIndex
+	for ($i = $firstDataRowIndex; $i -le $rowcnt; $i++) {
+		$time = $ws.Cells.Item($i, $timeColumnIndex).text 
+		$name = $ws.Cells.Item($i, $nameColumnIndex).text 
+		if ($name -eq "" -or $time -eq "") { continue }
+		$newPath = "$payrollFolder\${time}-工资单 $name.xlsx"
+		echo "{debug} generate $newPath"
+		$workbook = $Excel.Workbooks.Add()
+		$worksheet = $workbook.Worksheets.Item(1)
+		$wstmp.Copy($worksheet)
+		$worksheet = $workbook.Worksheets.Item(1)
+		$worksheet.Name = "Pay $name"
+
+		$ws.activate()
+		$srcRange = $ws.Rows.Item($i)
+		[void]$srcRange.copy()
+
+		$worksheet.activate()
+		$dstRange = $worksheet.Rows.Item($dstRowIdx)
+		$worksheet.paste($dstRange)
+
+		$selectRange = $worksheet.Cells.Item($dstRowIdx, 5)
+		[void]$selectRange.Select()
+
+		#[void]$workbook.Worksheets.Item(2).Delete()
+		$workbook.SaveAs($newPath)
+	}
+	[void]$wstmp.Delete()
+	$Excel.Workbooks.Close()
 }
-echo "{info} origsheet row count: $rowcnt"
-echo "{info} tempsheet row count: $tmprowcnt"
 
-$time = $ws.Cells.Item($firstDataRowIndex, $timeColumnIndex).text 
-$payrollFolder = "$pwd\payroll-list-$time"
-New-Item -Path $payrollFolder -ItemType Directory -ErrorAction Ignore >$null
-
-$dstRowIdx = $firstDataRowIndex
-for ($i = $firstDataRowIndex; $i -le $rowcnt; $i++) {
-	$time = $ws.Cells.Item($i, $timeColumnIndex).text 
-	$name = $ws.Cells.Item($i, $nameColumnIndex).text 
-	if ($name -eq "" -or $time -eq "") { continue }
-	$newPath = "$payrollFolder\${time}-工资单 $name.xlsx"
-	echo "{info} generate $newPath"
-	$workbook = $Excel.Workbooks.Add()
-	$worksheet = $workbook.Worksheets.Item(1)
-	$wstmp.Copy($worksheet)
-	$worksheet = $workbook.Worksheets.Item(1)
-	$worksheet.Name = "Pay $name"
-
-	$ws.activate()
-	$srcRange = $ws.Rows.Item($i)
-	[void]$srcRange.copy()
-
-	$worksheet.activate()
-	$dstRange = $worksheet.Rows.Item($dstRowIdx)
-	$worksheet.paste($dstRange)
-
-	$selectRange = $worksheet.Cells.Item($dstRowIdx, 5)
-	[void]$selectRange.Select()
-
-	#[void]$workbook.Worksheets.Item(2).Delete()
-	$workbook.SaveAs($newPath)
-}
-[void]$wstmp.Delete()
-$Excel.Workbooks.Close()
-
-$Excel.Quit()
+[void]$Excel.Quit()
 Stop-Excel
-
